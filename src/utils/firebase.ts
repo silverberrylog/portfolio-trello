1d2434ed5b827b0ca83472b8f9b2b5b15961cbeb
Gino Burdea <>
2022-05-06 19:53:35 +0300

    refactor: extract Firebase credentials to a .env file

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,16 +1,13 @@
 import { FirebaseOptions, initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-// https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig: FirebaseOptions = {
-    apiKey: 'AIzaSyD4Oi_G7VS9xBMPK7ahJIq5gCt-FJ19irk',
-    authDomain: 'portfolio-trello.firebaseapp.com',
-    databaseURL:
-        'https://portfolio-trello-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'portfolio-trello',
-    storageBucket: 'portfolio-trello.appspot.com',
-    messagingSenderId: '132905731762',
-    appId: '1:132905731762:web:f81a63205d8568408babab',
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
